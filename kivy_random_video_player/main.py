f014f95c11a880521fdb64571c6d73dbb3abb391
Robert_Schweppe <robert.schweppe@ufz.de>
2019-12-11 21:59:20 +0100

    - initial structure established based on tutorials of kivy

--- kivy_random_video_player/main.py
+++ kivy_random_video_player/main.py
@@ -3,12 +3,26 @@ kivy.require('1.11.1') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.lang import Builder
+from kivy.uix.videoplayer import VideoPlayer
 
+
+class MenuScreen(Screen):
+    pass
+
+class VideoScreen(Screen):
+    pass
+
+class MyScreenManager(ScreenManager):
+    pass
+
+main_kivy = Builder.load_file('video_player.kv')
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return main_kivy
 
 
 if __name__ == '__main__':
