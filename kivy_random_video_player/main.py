b9fc7b325d4ecb4b322a05933e48ab3cc0049323
Robert_Schweppe <robert.schweppe@ufz.de>
2019-12-16 22:28:51 +0100

    - implemented passing of video names and randomization - added button for bonus material

--- kivy_random_video_player/main.py
+++ kivy_random_video_player/main.py
@@ -1,20 +1,41 @@
 import kivy
-kivy.require('1.11.1') # replace with your current kivy version !
+
+kivy.require('1.11.1')  # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.uix.videoplayer import VideoPlayer
+from kivy.properties import StringProperty, ObjectProperty
+
+from pathlib import Path
 
 
 class MenuScreen(Screen):
-    pass
+    """"the screen with the main menu"""
+
+    # all the paths with the regular videos
+    video_paths = sorted(Path('data', 'resources').glob('*.mp4'))
+    # all the paths with the bonus material
+    bonus_paths = sorted(Path('data', 'bonus').glob('*.mp4'))
+    # the current video path
+    video_name = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(MenuScreen, self).__init__(**kwargs)
+
 
 class VideoScreen(Screen):
-    pass
+    video_name = StringProperty()
+    player = ObjectProperty()
 
-class MyScreenManager(ScreenManager):
-    pass
+    def __init__(self, **kwargs):
+        super(VideoScreen, self).__init__(**kwargs)
+
+    def on_leave(self, *args):
+        """stop the video if still running"""
+        self.player.state = 'stop'
+
 
 class Player(VideoPlayer):
     def __init__(self, **kwargs):
@@ -22,6 +43,7 @@ class Player(VideoPlayer):
 
 
 main_kivy = Builder.load_file('video_player.kv')
+
 
 class MyApp(App):
 
